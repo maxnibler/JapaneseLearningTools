c01b8341f39e6a9d32c5bd36d6b89bea15036689
Maximillian <max.nibler@gmail.com>
2020-11-25 20:42:42 -0800

    FrontEnd app init

---
+++ Frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import emails from '../data/kanji.json';
+
+/**
+ * Simple component with no state.
+ *
+ * See the basic-react from lecture 11 for an example of adding and
+ * reacting to changes in state and lecture 16 for details on Material-UI
+ *
+ * @return {object} JSX
+ */
+function App() {
+  const jpChar = 'h';
+  return (
+    <div>
+      {jpChar}
+    </div>
+  );
+}
+
+export default App;
