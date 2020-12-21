98b33432ee2f7d2c54228cf700c201abddecf0ca
Max <max.nibler@gmail.com>
2020-12-21 13:50:46 -0800

    Frontend Init

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import DimensionsProvider from './DimensionsProvider.js';
 import MobileView from './MobileView';
-import DesktopView from './DesktopView';
+// import DesktopView from './DesktopView';
 import ResponsiveLayout from './ResponsiveLayout';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,7 +27,7 @@ function App() {
       <DimensionsProvider>
         <ResponsiveLayout
           renderDesktop={() => (
-            <DesktopView/>
+            <MobileView/>
           )}
           renderMobile={() => (
             <MobileView/>
---
+++ Frontend/src/MobileView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+/**
+ * Simple component with no state.
+ *
+ * @return {object} JSX
+ */
+function MobileView() {
+  return (
+    <div>
+      MobileView
+    </div>
+  )
+}
+
+export default MobileView
