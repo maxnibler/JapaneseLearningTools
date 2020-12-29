cfe9930d65ff928ae64e7293ad9cd8da95301fd7
Max <max.nibler@gmail.com>
2020-12-29 13:48:58 -0800

    Formatted bottom Cards

--- Frontend/src/Pages.js
+++ Frontend/src/Pages.js
@@ -4,23 +4,37 @@ import { Box, Paper, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    height: window.innerHeight,
+    width: window.innerWidth,
+  },
+  topRoot: {
     display: 'flex',
     flexGrow: 3,
     justifyContent: 'center',
-    width: window.innerWidth,
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
+    width: '100%',
   },
-  paper: {
+  bottomRoot: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  paperKanji: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    width: theme.spacing(16),
+    height: theme.spacing(16),
   },
   kanji: {
     fontSize: '30pt',
+  },
+  paperCard: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: theme.spacing(16),
   },
 }));
 
@@ -32,12 +46,20 @@ function kanjiDisplay(character) {
   const classes = useStyles();
 
   return (
-    <div>
-      <Box className={classes.root}>
-        <Paper elevation={2} className={classes.paper}>
+    <div className={classes.root}>
+      <Box className={classes.topRoot}>
+        <Paper elevation={2} className={classes.paperKanji}>
           <Typography className={classes.kanji}>
             {character}
           </Typography>
+        </Paper>
+      </Box>
+      <Box className={classes.bottomRoot}>
+        <Paper className={classes.paperCard}>
+          pronuciation
+        </Paper>
+        <Paper className={classes.paperCard}>
+          pronuciation
         </Paper>
       </Box>
     </div>
